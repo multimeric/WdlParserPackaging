caf2f788719d63a04fb5f974dd14e36e963fae81
Michael Milton <ttmigueltt@gmail.com>
2018-12-19 14:26:25 +1100

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@
 from setuptools import setup
 
 setup(
+    maintainer="TMiguelT",
+    maintainer_email="ttmigueltt@gmail.com",
+    description="Python package, containing Python parsers for the Broad's WDL language",
+    license="GPL",
+    test_suite="test",
+    url="https://github.com/TMiguelT/WdlParserPackaging",
     name='wdl_parser',
     packages=['wdl_parser'],
     version='0.0.1',
